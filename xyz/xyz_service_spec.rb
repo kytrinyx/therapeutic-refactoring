d959ddf8c53dcaae434cd4f5f1ea6b679c3b8892
Katrina Owen <katrina.owen@gmail.com>
2012-06-09 13:22:29 +0200

    Add missing input for kind on stub.

--- xyz/xyz_service_spec.rb
+++ xyz/xyz_service_spec.rb
@@ -5,7 +5,8 @@ describe XYZService do
   let(:target) do
     messages = {
       :publish_on => Date.new(2012, 3, 14),
-      :xyz_category_prefix => 'abc'
+      :xyz_category_prefix => 'abc',
+      :kind => 'unicorn'
     }
     stub(:target, messages)
   end
