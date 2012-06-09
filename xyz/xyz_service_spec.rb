35696313a2b3bcbe519ab9f20b842ac8d728b12e
Katrina Owen <katrina.owen@gmail.com>
2012-06-09 13:39:57 +0200

    Add test case to cover titles too short to get truncated.

--- xyz/xyz_service_spec.rb
+++ xyz/xyz_service_spec.rb
@@ -37,4 +37,9 @@ describe XYZService do
     subject.should match(/07abcmagicunicorn_000_1337_[0-9a-f]{8}_isparklysp\.jpg/)
   end
 
+  it 'handles short titles' do
+    target.stub(:title => 'O HAI')
+    subject.should match(/07abcmagicunicorn_1337_[0-9a-f]{8}_ohai\.jpg/)
+  end
+
 end
