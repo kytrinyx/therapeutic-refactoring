f2483571f3c286224e18b01303fd9df4c35b47b0
Katrina Owen <katrina.owen@gmail.com>
2012-06-09 13:50:39 +0200

    Tidy up truncated title method

--- xyz/xyz_file.rb
+++ xyz/xyz_file.rb
@@ -9,18 +9,24 @@ class XYZFile
     filename = "#{target.publish_on.strftime("%d")}"
     filename << "#{target.xyz_category_prefix}"
     filename << "#{target.kind.gsub("_", "")}"
-    filename << "_%03d" % (target.age || 0) if target.personal?
+    filename << "_#{age}" if target.personal?
     filename << "_#{target.id.to_s}"
-    filename << "_#{Digest::SHA1.hexdigest(rand(10000).to_s)[0,8]}"
+    filename << "_#{noise}"
     filename << "_#{truncated_title}"
     filename << ".jpg"
     return filename
   end
 
   def truncated_title
-    truncated_title = target.title.gsub(/[^\[a-z\]]/i, '').downcase
-    truncate_to = truncated_title.length > 9 ? 9 : truncated_title.length
-    truncated_title[0..(truncate_to)]
+    target.title.downcase.gsub(/[^\[a-z\]]/, '')[0..9]
+  end
+
+  def noise
+    Digest::SHA1.hexdigest(rand(10000).to_s)[0,8]
+  end
+
+  def age
+    "%03d" % (target.age || 0)
   end
 
 end
