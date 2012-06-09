77a1d8103d007653a869e88b7a5005efbf8ea2e3
Katrina Owen <katrina.owen@gmail.com>
2012-06-09 13:55:06 +0200

    Reorder methods.
    This is simply for aesthetic purposes so that the methods are listed in
    the same order that they are called.

--- xyz/xyz_file.rb
+++ xyz/xyz_file.rb
@@ -6,9 +6,9 @@ class XYZFile
   end
 
   def name
-    filename = "#{publication_day}"
-    filename << "#{category}"
-    filename << "#{kind}"
+    filename = publication_day
+    filename << category
+    filename << kind
     filename << "_#{age}" if target.personal?
     filename << "_#{target.id.to_s}"
     filename << "_#{noise}"
@@ -29,16 +29,16 @@ class XYZFile
     target.kind.gsub("_", "")
   end
 
-  def truncated_title
-    target.title.downcase.gsub(/[^\[a-z\]]/, '')[0..9]
+  def age
+    "%03d" % (target.age || 0)
   end
 
   def noise
     Digest::SHA1.hexdigest(rand(10000).to_s)[0,8]
   end
 
-  def age
-    "%03d" % (target.age || 0)
+  def truncated_title
+    target.title.downcase.gsub(/[^\[a-z\]]/, '')[0..9]
   end
 
 end
