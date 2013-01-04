63c3f8160eb2ba134906c477cca2b80b43990559
simonkro <simonkroeger@gmx.de>
2013-01-04 22:34:40 +0100

    a method is a method is a method ...
    ... not a class

--- xyz/xyz_service.rb
+++ xyz/xyz_service.rb
@@ -1,9 +1,17 @@
-require_relative './xyz_file'
+require 'securerandom'
 
 module XYZService
 
   def self.xyz_filename(target)
-    XYZFile.new(target).name
+    day =  target.publish_on.strftime('%d')
+    category = target.xyz_category_prefix
+    kind = target.kind.delete('_')
+    
+    age = '_%03i' % target.age.to_i if target.personal?
+    noise = SecureRandom.hex(4)
+    title = target.title.downcase.delete('^a-z[]')[0..9]
+    
+    "#{day}#{category}#{kind}#{age}_#{target.id}_#{noise}_#{title}.jpg"
   end
 
 end
